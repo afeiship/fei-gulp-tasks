18b2fd5eeb225e6e600425834cecd4792d9f66ef
afeiship <1290657123@qq.com>
2018-02-03 19:08:46 +0800

    issue: add version task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
-(function () {
+(function() {
 
+  'use strict';
+
+  var path = require('path');
   var gulp = require('gulp');
-  var wrench = require('wrench');
-
-  /**
-   *  This will load all js or coffee files in the gulp directory
-   *  in order to load all gulp tasks
-   */
-  wrench.readdirSyncRecursive('./gulp').map(function (file) {
-    require('./gulp/' + file);
+  var argv = require('yargs').argv;
+  var fs = require('fs');
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
   });
 
+  //import
+  fs.readdirSync('./build').map(function(file) {
+    require('./build/' + file);
+  });
+
+
+  gulp.task('default',['build']);
 
 }());
